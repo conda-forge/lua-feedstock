7c7bba501366d14ee7277e573dd96231db91a4d3
Andre Anjos <andre.dos.anjos@gmail.com>
2025-04-01 17:10:41 +0200

    recipe: streamline changes and test locally

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,46 +1,22 @@
 #!/usr/bin/env bash
 
-if [ `uname` == Linux ]; then
-    make INSTALL_TOP=$PREFIX \
-         CC="${CC}" \
-         MYCFLAGS="${CLFAGS} -fPIC -I$PREFIX/include -L$PREFIX/lib  -DLUA_USE_DLOPEN -DLUA_USE_LINUX -DLUA_USER_DEFAULT_PATH='\"$PREFIX/\"'" \
-         MYLDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath=$PREFIX/lib" \
-         linux
-elif [ `uname` == Darwin ]; then
-    make \
-    CC="${CC}" \
-    INSTALL_TOP="${PREFIX}" \
-    MYCFLAGS="${CLFAGS} -fPIC -I$PREFIX/include -L$PREFIX/lib ${LUA_CFLAGS}" \
-    MYLDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib" \
-    LUA_SO="liblua${SHLIB_EXT}" \
-    macosx
+if [ $(uname) == Linux ]; then
+    make CC="${CC}" INSTALL_TOP="${PREFIX}" LUA_SO="liblua${SHLIB_EXT}" linux
+elif [ $(uname) == Darwin ]; then
+    make CC="${CC}" INSTALL_TOP="${PREFIX}" LUA_SO="liblua${SHLIB_EXT}" macosx
 fi
 
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
-    make \
-        CC="${CC}" \
-        LUA_SO="liblua${SHLIB_EXT}" \
-        test
+    make CC="${CC}" INSTALL_TOP="${PREFIX}" LUA_SO="liblua${SHLIB_EXT}" test
 fi
 
-# If that static library is ever needed, "liblua.a" needs to be added to TO_LIB
-if [ "$(uname)" == "Darwin" ]; then
-    TO_LIB="liblua${SHLIB_EXT} liblua.${PKG_VERSION%.*}${SHLIB_EXT} liblua.${PKG_VERSION}${SHLIB_EXT}"
-else
-    TO_LIB="liblua${SHLIB_EXT} liblua${SHLIB_EXT}.${PKG_VERSION%.*} liblua${SHLIB_EXT}.${PKG_VERSION}"
-fi
-
-make \
-    INSTALL_TOP="$PREFIX" \
-    LUA_SO="liblua${SHLIB_EXT}" \
-    TO_LIB="${TO_LIB}" \
-    install
+make INSTALL_TOP="${PREFIX}" LUA_SO="liblua${SHLIB_EXT}" install
 
 # Create the pkg-config file
 mkdir -p "${PREFIX}/lib/pkgconfig"
-(sed -e "s@PKG_VERSION@${PKG_VERSION}@g" -e "s@CONDA_PREFIX@${PREFIX}@g" | \
- sed -E "s@^(V=.+)\.[0-9]+@\1@g" \
- > "${PREFIX}/lib/pkgconfig/lua.pc") << "EOF"
+(sed -e "s@PKG_VERSION@${PKG_VERSION}@g" -e "s@CONDA_PREFIX@${PREFIX}@g" |
+    sed -E "s@^(V=.+)\.[0-9]+@\1@g" \
+        >"${PREFIX}/lib/pkgconfig/lua.pc") <<"EOF"
 V=PKG_VERSION
 R=PKG_VERSION
 
@@ -59,6 +35,6 @@ Name: Lua
 Description: An Extensible Extension Language
 Version: ${R}
 Requires:
-Libs: -L${libdir} -llua -lm -ldl
+Libs: -L${libdir} -llua -lm
 Cflags: -I${includedir}
 EOF
